5f86347b4bb8c22a90d54cb7755b816fb9e693a5
IvanBezrukavyi <ivanbezrukavyi1@gmail.com>
2023-10-12 14:34:18 +0300

    third commit

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,7 +1,20 @@
 //Difine and import module for dealing with tests
 const { test } = require('@playwright/test');
+const { expect} = require('@playwright/test');
 
 //we have to use async function because this is JS and add browser parameter
-test('My first test. This is a TC name', async ({browser}) => {
-    const page = await browser.newPage();
+test('Browser context test', async ({ browser }) => {
+    //ignore browser's plugin/cookies and run browser in incognito mode
+    const context = await browser.newContext();
+    //Open new browser page
+    const page = await context.newPage();
+    //Navigate to needed page
+    const mainPage = await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+});
+
+test('Simpler than previous test', async ({ page }) => {
+   await page.goto('https://demoqa.com/');
+   // Get title - assertion
+   console.log(await page.title());
+   await expect(page).toHaveTitle(/.*DEMOQA/);
 });
