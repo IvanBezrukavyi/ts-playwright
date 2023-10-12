9d7e1e020c3b36c3870a03829d263d0d2e871fa3
IvanBezrukavyi <ivanbezrukavyi1@gmail.com>
2023-10-12 14:28:39 +0300

    add two tests

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,6 +1,7 @@
+//Difine and import module for dealing with tests
 const { test } = require('@playwright/test');
 
-
+//we have to use async function because this is JS and add browser parameter
 test('My first test. This is a TC name', async ({browser}) => {
     const page = await browser.newPage();
 });
