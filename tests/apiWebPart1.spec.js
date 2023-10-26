f72b3c5a194ce8e88a06e83399eb6c2b9fb462af
IvanBezrukavyi <ivanbezrukavyi1@gmail.com>
2023-10-26 13:12:24 +0300

    refactoring

--- tests/apiWebPart1.spec.js
+++ tests/apiWebPart1.spec.js
@@ -1,117 +1,38 @@
 const { test, expect, request } = require("@playwright/test");
+const {APIUtils} = require('./utils/APIUtils');
 
-let token;
+let response;
 
-const loginPayload = {
-  userEmail: "nspprotest@gmail.com",
-  userPassword: "Pl@ywright_test_m1",
+
+const orderPayload = {
+  orders: [
+    {country: "Ukraine",
+    productOrderedId: "6262e9d9e26b7e1a10e89c04"
+}]
 };
 
 test.beforeAll( async () => {
   const apiContext = await request.newContext();
-  const loginResponse = await apiContext.post(
-    "https://rahulshettyacademy.com/api/ecom/auth/login",
-    {
-      data: loginPayload,
-    }
-  );
-  expect(loginResponse.ok()).toBeTruthy();
-  expect(loginResponse.status()).toBe(200);
-  const loginResponseToken = await loginResponse.json();
-  token = loginResponseToken.token;
-
-  console.log("LOG: Token: ", token);
-  
+  const apiUtils = new APIUtils(apiContext, loginPayload);
+  response = apiUtils.createOrder(orderPayload);
 });
 
-test("E2E for ordering IPHONE 13 PRO cell phone", async ({ page }) => {
-  await page.goto("https://rahulshettyacademy.com/client");
-  //Add extracted user token to web browser local storage
+test("E2E for ordering IPHONE 13 PRO cell phone with mix UI and API", async ({ page }) => {
+  const apiUtils = new ApiUtils(apiContext, loginPayload);
+  const orderId = new createOrder(orderPayload);
+  console.log("Verify success login");
+  expect(loginResponse.ok()).toBeTruthy();
+  expect(loginResponse.status()).toBe(200);
+  console.log("Verify success ordering of stuff");
+  expect(createdOrderResponse.ok()).toBeTruthy();
+  expect(createdOrderResponse.status()).toBe(201);
   page.addInitScript(value => 
     {
       window.localStorage.setItem('token', value);
-    }, token);
+    }, response.token);
   
-  await page.goto("https://rahulshettyacademy.com/client");  
-  //User Data
-  const cvv = "186";
-  const cardName = "My test Visa Card";
-  const products = page.locator(".card-body");
-  const productName = "iphone 13 pro";
-  const email = "nspprotest@gmail.com";
-  //Common locators
-  const cardTitle = page.locator(".card-body b");
-  const cardTitles = page.locator(".card-body b");
-  const checkoutBtn = page.locator("[routerlink*=cart]");
-
-  console.log(await cardTitle.first().textContent());
-  console.log(
-    "LOG: Product list titles is: ",
-    await cardTitles.allTextContents()
-  );
-  console.log("LOG: Checkout counter equals to 0");
-  await expect(checkoutBtn).toContainText("");
-  await checkoutBtn.textContent();
-  const count = await products.count();
-  /* The code snippet is iterating over a list of products and checking if the product name matches the
-    desired product name (in this case, "iphone 13 pro"). If a match is found, it clicks on the "Add
-    To Cart" button for that product and then breaks out of the loop. This code is essentially adding
-    the desired product to the shopping cart. */
-  for (let i = 0; i < count; ++i) {
-    // eslint-disable-next-line playwright/no-conditional-in-test
-    if ((await products.nth(i).locator("b").textContent()) === productName) {
-      // Add to cart
-      await products.nth(i).locator("text= Add To Cart").click();
-      break;
-    }
-  }
-  //await page.locator(checkoutBtn).first().waitFor();
-  console.log("LOG: Checkout counter has been changed");
-  await expect(checkoutBtn).not.toBeNull();
-  await checkoutBtn.click();
-  // Checkout page
-  await page.locator("div li").first().waitFor();
-  const presentedItem = await page
-    .locator("div[class='cartSection'] h3")
-    .isVisible();
-  await expect(presentedItem).toBeTruthy();
-  console.log("LOG: Item is presented in Checkout page: ", presentedItem);
-  console.log("LOG: Click checkout button");
-  await page.locator("button[type='button']").last().click();
-  //Order page
-  await page.locator("[placeholder*='Country']").type("ukr", { delay: 100 });
-  //Declare the country drop-down list
-  const countryDropDown = page.locator(".ta-results");
-  /* The code snippet is selecting the country "Ukraine" from a dropdown list. */
-  await countryDropDown.waitFor();
-  const optionsCount = await countryDropDown.locator("button").count();
-  for (let i = 0; i < optionsCount; i++) {
-    const text = await countryDropDown.locator("button").nth(i).textContent();
-    // eslint-disable-next-line playwright/no-conditional-in-test
-    if (text.trim() == "Ukraine") {
-      await countryDropDown.locator("button").nth(i).click();
-      break;
-    }
-  }
-  console.log("Enter personal information");
-  await page.locator("(//input[@type='text'])[2]").fill(cvv);
-  await page.locator("(//input[@type='text'])[3]").fill(cardName);
-  console.log("Verify shipping information");
-  const userEmail = page.locator("label[type='text']");
-  await userEmail.waitFor("visible");
-  await expect(userEmail).toHaveText(email);
-  console.log("LOG: Click place order button");
-  await page.locator(".action__submit").click();
-  console.log("Review completed order");
-  await expect(page.locator(".hero-primary")).toHaveText(
-    " Thankyou for the order. "
-  );
-  const orderId = await page
-    .locator(".em-spacer-1 .ng-star-inserted")
-    .textContent();
-  console.log(orderId);
-  await expect(page.locator(".em-spacer-1 .ng-star-inserted")).not.toBeEmpty();
-  await page.locator("button[routerlink*=myorders]").click();
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.locator("button[routerlink*=myorders]").click();  
   /*This ensures that the table is loaded
       and available for further actions or assertions. */
   await page.locator("table").waitFor();
@@ -119,11 +40,11 @@ test("E2E for ordering IPHONE 13 PRO cell phone", async ({ page }) => {
   for (let i = 0; i < (await rows.count()); ++i) {
     const rowOrderId = await rows.nth(i).locator("th").textContent();
     // eslint-disable-next-line playwright/no-conditional-in-test
-    if (orderId.includes(rowOrderId)) {
+    if (response.orderId.includes(rowOrderId)) {
       await rows.nth(i).locator("button").first().click();
       break;
     }
   }
   const orderIdDetails = await page.locator(".col-text").textContent();
-  expect(orderId.includes(orderIdDetails)).toBeTruthy();
+  expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 });
---
+++ tests/utils/APIUtils.js
@@ -0,0 +1,43 @@
+class APIUtils
+{
+    constructor(apiContext, loginPayload)
+    {
+        this.apiContext = apiContext;
+        this.loginPayload = loginPayload;
+
+    }
+    async getToken()
+    {
+        const loginResponse = await this.apiContext.post(
+            "https://rahulshettyacademy.com/api/ecom/auth/login",
+            {
+              data: this.loginPayload,
+            }
+          );
+          const loginResponseToken = await loginResponse.json();
+          token = loginResponseToken.token;
+          console.log("LOG: Token: ", token);
+          return token;
+    }
+    async createOrder(orderPayload)
+    {
+        let response = {};
+        response.token = await this.getToken();
+        const createdOrderResponse = await this.apiContext.post(
+            "https://rahulshettyacademy.com/api/ecom/order/create-order",
+          {  
+            data: orderPayload,
+            headers: {
+              'Authorization': response.token,
+              'Content-type': 'application/json'
+            },
+          });
+          const oderResponseInfo = await createdOrderResponse.json();
+          const orderId = oderResponseInfo.orders[0];
+          console.log("LOG: Product Order ID: ", orderId);
+          response.orderId = orderId;
+          return response;
+    }
+}
+
+module.exports = {APIUtils};
