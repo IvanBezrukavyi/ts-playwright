d8fc78f7aa52da28a519e94631171c762a3559bb
IvanBezrukavyi <ivanbezrukavyi1@gmail.com>
2023-11-09 10:01:41 +0200

    attempt to fix tests2

--- tests/networkTest.spec.ts
+++ tests/networkTest.spec.ts
@@ -25,7 +25,9 @@ const loginPayload: LoginPayload = {
 
 const orderPayload: OrderPayload = {
   orders: [
-    { country: "Ukraine", productOrderedId: "6262e9d9e26b7e1a10e89c04" },
+    { country: "Ukraine",
+      productOrderedId: "6262e9d9e26b7e1a10e89c04"
+    },
   ],
 };
 
@@ -37,40 +39,42 @@ const fakePayloadOrders: FakePayloadOrders = {
 let response: { token: string; orderId: number };
 
 test.beforeAll(async () => {
-  const apiContext = await request.newContext();
-  const apiUtils = new ApiUtils(apiContext, loginPayload);
-  response = await apiUtils.createOrder(orderPayload);
-  console.log("Verify success login");
+  try {
+    const apiContext = await request.newContext();
+    const apiUtils = new ApiUtils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
+    console.log("Successfully obtained token and orderId");
+  } catch (error) {
+    console.error("Error during beforeAll test:", error);
+    throw error;
+  }
 });
 
-test("TC: Verify absence of order via intercepted request", async ({
-  page,
-}) => {
-  await page.addInitScript((value) => {
-    window.localStorage.setItem("token", value);
-  }, response.token);
+test("TC: Verify absence of order via intercepted request", async ({ page }) => {
+  try {
+    await page.addInitScript((value) => {
+      window.localStorage.setItem("token", value);
+    }, response.token);
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.route(
+      "https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
+      async (route) => {
+        const response = await page.request.fetch(route.request());
+        let body = JSON.stringify(fakePayloadOrders);
 
-  await page.goto("https://rahulshettyacademy.com/client");
-
-  await page.route(
-    "https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
-    async (route) => {
-      const response = await page.request.fetch(route.request());
-      let body = JSON.stringify(fakePayloadOrders);
-
-      route.fulfill({
-        response,
-        body,
-      });
-    }
-  );
-
-  await page.locator("button[routerlink*=myorders]").click();
-
-  console.log("LOG: No Orders in the cart");
-
-  await expect(page.locator(".mt-4")).toHaveText(
-    "You have No Orders to show at this time.\
-    Please Visit Back Us"
-  );
+        route.fulfill({
+          response,
+          body,
+        });
+      }
+    );
+    await page.locator("button[routerlink*=myorders]").click();
+    console.log("LOG: No Orders in the cart");
+    await expect(page.locator(".mt-4")).toHaveText(
+      "You have No Orders to show at this time. Please Visit Back Us"
+    );
+  } catch (error) {
+    console.error("Error during test:", error);
+    throw error;
+  };
 });
