29bf923643fa98424614fd0b6c2427c99c514801
IvanBezrukavyi <ivanbezrukavyi1@gmail.com>
2023-11-28 13:05:06 +0200

    install faker lib and add declare needed consts

--- tests-demoqa/textBoxAndFaker.spec.ts
+++ tests-demoqa/textBoxAndFaker.spec.ts
@@ -1,7 +1,16 @@
-import {test, expect} from "playwright/test";
+import { test, expect } from "playwright/test";
+import { faker } from "@faker-js/faker/locale/en";
 
-test('TC: Fill out input by fake data', async({page}) => {
-    await page.goto('/');
+test.describe("Randomize test data by fake data", () => {
+  test("TC: Fill out input text fields by data from faker lib", async ({
+    page,
+  }) => {
+    const fullName = faker.person.fullName();
+    const email = faker.internet.email({provider: 'example.ibez.com'});
+    const currentAddress = faker.location.secondaryAddress();
+    const permanentAddress = faker.location.secondaryAddress();
+
+    await page.goto("/");
     await page.click("//h5[normalize-space()='Elements']");
-    
-})
+  });
+});
